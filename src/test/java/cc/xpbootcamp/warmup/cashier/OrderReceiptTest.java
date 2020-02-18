54ac390e35839e8ce37f0677391a38c0d2bcbbae
superPershing <727342027@qq.com>
2020-02-18 23:38:08 +0800

    update test

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -2,41 +2,53 @@ package cc.xpbootcamp.warmup.cashier;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class OrderReceiptTest {
-    @Test
-    void shouldPrintCustomerInformationOnOrder() {
-        Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<Product>());
-        OrderReceipt receipt = new OrderReceipt(order);
-
-        String output = receipt.printReceipt();
-
-
-        assertThat(output, containsString("Mr X"));
-        assertThat(output, containsString("Chicago, 60601"));
-    }
 
     @Test
-    public void shouldPrintLineItemAndSalesTaxInformation() {
+    public void shouldPrintReceiptWithDiscountWhenWeekDayIsWednesday() {
         List<Product> products = new ArrayList<Product>() {{
-            add(new Product("milk", 10.0, 2));
-            add(new Product("biscuits", 5.0, 5));
-            add(new Product("chocolate", 20.0, 1));
+            add(new Product("巧克力", 21.50, 2));
+            add(new Product("小白菜", 10.00, 1));
         }};
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, products));
+        LocalDate date = LocalDate.of(2020, 2, 19);
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, products, date));
 
         String output = receipt.printReceipt();
-
-        assertThat(output, containsString("milk\t10.0\t2\t20.0\n"));
-        assertThat(output, containsString("biscuits\t5.0\t5\t25.0\n"));
-        assertThat(output, containsString("chocolate\t20.0\t1\t20.0\n"));
-        assertThat(output, containsString("Sales Tax\t6.5"));
-        assertThat(output, containsString("Total Amount\t71.5"));
+        assertThat(output).isEqualTo("===== 老王超市，值得信赖 ======\n" +
+                "\n" +
+                "2020年2月19日，星期三\n" +
+                "\n" +
+                "巧克力, 21.50 x 2, 43.00\n" +
+                "小白菜, 10.00 x 1, 10.00\n" +
+                "-----------------------------------\n" +
+                "税额: 5.30\n" +
+                "折扣: 1.17\n" +
+                "总价: 57.13\n");
     }
+    @Test
+    public void shouldPrintReceiptWithDiscountWhenWeekDayIsNotWednesday() {
+        List<Product> products = new ArrayList<Product>() {{
+            add(new Product("巧克力", 21.50, 2));
+            add(new Product("小白菜", 10.00, 1));
+        }};
+        LocalDate date = LocalDate.of(2020, 2, 17);
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, products, date));
 
+        String output = receipt.printReceipt();
+        assertThat(output).isEqualTo("===== 老王超市，值得信赖 ======\n" +
+                "\n" +
+                "2020年2月19日，星期三\n" +
+                "\n" +
+                "巧克力, 21.50 x 2, 43.00\n" +
+                "小白菜, 10.00 x 1, 10.00\n" +
+                "-----------------------------------\n" +
+                "税额: 5.30\n" +
+                "总价: 58.30\n");
+    }
 }
