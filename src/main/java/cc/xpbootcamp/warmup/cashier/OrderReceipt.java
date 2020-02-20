a55f0219718cc947a00b7e010367365a823b1692
superPershing <727342027@qq.com>
2020-02-20 20:48:41 +0800

    [refactor](OrderReciept): rewrite DateTimeFormatter

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,9 +1,9 @@
 package cc.xpbootcamp.warmup.cashier;
 
 import java.math.RoundingMode;
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.HashMap;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class OrderReceipt {
@@ -37,7 +37,7 @@ public class OrderReceipt {
 
     private String buildDateInfo() {
         LocalDate orderDate = order.getPurchasedDate();
-        return String.format("%d年%d月%d日，%s%n", orderDate.getYear(), orderDate.getMonthValue(), orderDate.getDayOfMonth(), WeekDay.fromDay(orderDate.getDayOfWeek()).getDesc());
+        return String.format("%s%n", DateTimeFormatter.ofPattern("yyyy年M月dd日，EEEE", Locale.CHINA).format(orderDate));
     }
 
     private String buildProductsInfo() {
@@ -47,7 +47,6 @@ public class OrderReceipt {
     private String buildSeparateLine() {
         return "-----------------------------------\n";
     }
-
 
     private String buildProductInfo(LineItem lineItem) {
         return String.format("%s, %s x %d, %s%n", lineItem.getDescription(), lineItem.getPrice().setScale(AMOUNT_SCALE, RoundingMode.HALF_UP), lineItem.getQuantity(), lineItem.totalAmount().setScale(AMOUNT_SCALE, RoundingMode.HALF_UP));
@@ -66,40 +65,5 @@ public class OrderReceipt {
 
     private String buildTotalAmountInfo() {
         return String.format("总价: %s%n", order.getTotalCost().setScale(AMOUNT_SCALE, RoundingMode.HALF_UP));
-    }
-
-    public enum WeekDay {
-        SUNDAY(DayOfWeek.SUNDAY, "星期日"),
-        MONDAY(DayOfWeek.MONDAY, "星期一"),
-        TUESDAY(DayOfWeek.TUESDAY, "星期二"),
-        WEDNESDAY(DayOfWeek.WEDNESDAY, "星期三"),
-        THURSDAY(DayOfWeek.THURSDAY, "星期四"),
-        FRIDAY(DayOfWeek.FRIDAY, "星期五"),
-        SATURDAY(DayOfWeek.SATURDAY, "星期六");
-
-        private static HashMap<DayOfWeek, WeekDay> hashMap = new HashMap<>();
-
-        static {
-            for (WeekDay day : WeekDay.values()) {
-                hashMap.put(day.dayOfWeek, day);
-            }
-
-        }
-
-        private DayOfWeek dayOfWeek;
-        private String desc;
-
-        WeekDay(DayOfWeek dayOfWeek, String desc) {
-            this.dayOfWeek = dayOfWeek;
-            this.desc = desc;
-        }
-
-        public static WeekDay fromDay(DayOfWeek dayOfWeek) {
-            return hashMap.get(dayOfWeek);
-        }
-
-        public String getDesc() {
-            return desc;
-        }
     }
 }
